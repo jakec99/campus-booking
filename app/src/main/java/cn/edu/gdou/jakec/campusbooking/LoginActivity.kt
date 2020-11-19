98c8c3fb55aa61e22fb276fd0ff5c34f623fb598
Jake Chung <jakec99@foxmail.com>
2020-11-20 01:19:28 +0800

    LoginActivity

--- app/src/main/java/cn/edu/gdou/jakec/campusbooking/LoginActivity.kt
+++ app/src/main/java/cn/edu/gdou/jakec/campusbooking/LoginActivity.kt
@@ -2,10 +2,60 @@ package cn.edu.gdou.jakec.campusbooking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import cn.edu.gdou.jakec.campusbooking.databinding.ActivityLoginBinding
+import com.google.android.material.snackbar.Snackbar
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        binding.closeButton.setOnClickListener {
+            Snackbar.make(it, "Don't want to close it.", Snackbar.LENGTH_SHORT)
+                .show()
+        }
+
+        binding.sendButton.setOnClickListener {
+            Snackbar.make(it, "Pretend to send a text.", Snackbar.LENGTH_SHORT)
+                .show()
+        }
+
+        binding.registerButton.setOnClickListener {
+            Snackbar.make(it, "Maybe login.", Snackbar.LENGTH_SHORT)
+                .show()
+        }
+
+        binding.loginButton.setOnClickListener {
+            Snackbar.make(it, "Waiting password.", Snackbar.LENGTH_SHORT)
+                .show()
+        }
+
+        binding.passwordButton.setOnClickListener {
+            binding.codeTextField.setVisibility(View.GONE);
+            binding.sendButton.setVisibility(View.GONE);
+            binding.registerButton.setVisibility(View.GONE);
+            binding.orTextView.setVisibility(View.GONE);
+            binding.passwordButton.setVisibility(View.GONE);
+            binding.backButton.setVisibility(View.VISIBLE);
+            binding.passwordTextField.setVisibility(View.VISIBLE);
+            binding.loginButton.setVisibility(View.VISIBLE);
+        }
+
+        binding.backButton.setOnClickListener {
+            binding.backButton.setVisibility(View.GONE);
+            binding.passwordTextField.setVisibility(View.GONE);
+            binding.loginButton.setVisibility(View.GONE);
+            binding.codeTextField.setVisibility(View.VISIBLE);
+            binding.sendButton.setVisibility(View.VISIBLE);
+            binding.registerButton.setVisibility(View.VISIBLE);
+            binding.orTextView.setVisibility(View.VISIBLE);
+            binding.passwordButton.setVisibility(View.VISIBLE);
+        }
     }
 }
--- app/src/main/java/cn/edu/gdou/jakec/campusbooking/MainActivity.kt
+++ app/src/main/java/cn/edu/gdou/jakec/campusbooking/MainActivity.kt
@@ -2,10 +2,18 @@ package cn.edu.gdou.jakec.campusbooking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import cn.edu.gdou.jakec.campusbooking.databinding.ActivityLoginBinding
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContentView(R.layout.activity_login)
     }
 }
